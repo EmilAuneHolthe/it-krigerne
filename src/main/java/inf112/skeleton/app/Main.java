611befdf6bea6db1488235a0be21e6aae0046a4f
Emhol8004 <Emhol8004@uib.no>
2025-02-28 16:11:49 +0100

    Game loop and key input

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -1,23 +1,28 @@
 package inf112.skeleton.app;
 
+import javax.swing.JFrame;
 
-import org.lwjgl.system.Configuration;
+import com.badlogic.gdx.Game;
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.badlogic.gdx.utils.SharedLibraryLoader;
-import com.badlogic.gdx.utils.Os;
-import com.badlogic.gdx.utils.SharedLibraryLoader;
 
 public class Main {
-	public static void main(String[] args) {
-		if (SharedLibraryLoader.os == Os.MacOsX) {
-			Configuration.GLFW_LIBRARY_NAME.set("glfw_async");
-		}
-		Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
-		cfg.setTitle("hello-world");
-		cfg.setWindowedMode(480, 320);
+    public static void main(String[] args) {
 
-		new Lwjgl3Application(new HelloWorld(), cfg);
-	}
+       JFrame window = new JFrame();
+       window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       window.setResizable(false);
+       window.setTitle("Stairs");
+
+       GamePanel gamePanel = new GamePanel();
+       window.add(gamePanel); 
+       window.pack();  
+
+    
+       
+       window.setLocationRelativeTo(null);
+       window.setVisible(true);
+
+       gamePanel.startGameThread();
+    
+    }
 }
