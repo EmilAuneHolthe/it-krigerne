a9919aa2141af664f8860d4cb9f531f71f2e3ad9
Mona Lisa <email@example.com>
2025-03-12 11:05:24 +0100

    Endringer I keyHandler (glemte å bruke keyHandler brachen)

--- src/main/java/inf112/skeleton/controller/KeyHandler.java
+++ src/main/java/inf112/skeleton/controller/KeyHandler.java
@@ -1,75 +1,61 @@
 package inf112.skeleton.controller;
 
-import java.awt.event.KeyListener;
+import com.badlogic.gdx.InputProcessor;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.math.Rectangle;
-
-import java.awt.event.KeyEvent;
-
-public class KeyHandler implements KeyListener {
-
-    public boolean up, down, left, right;
-    private float dx = 1, dy = 1;
-    private Rectangle spriteRect;
+public class KeyHandler implements InputProcessor {
 
     @Override
-    public void keyTyped(KeyEvent e) {
-     
-        
+    public boolean keyDown(int keycode) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyDown'");
     }
 
     @Override
-    public void keyPressed(KeyEvent e) {
-        
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-			dy = -10;
-			spriteRect.y += dy;
-			dy = 0;
-		}
-		else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-			dy = 10;
-			spriteRect.y += dy;
-			dy = 0;
-		
-		}
-		else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			dx = -10;
-			spriteRect.x += dx;
-			dx = 0;
-		
-		}
-		else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			dx = 10;
-			spriteRect.x += dx;
-			dx = 0;
-		}
+    public boolean keyUp(int keycode) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyUp'");
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
-        
-        int code = e.getKeyCode();
+    public boolean keyTyped(char character) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
 
-        switch (code) {
-            case KeyEvent.VK_UP:
-                up = false;  
-                break;
-            case KeyEvent.VK_DOWN:
-                down = false;
-                break;
-            case KeyEvent.VK_LEFT:
-                left = false;
-                break;
-            case KeyEvent.VK_RIGHT:
-                right = false;
-                break;
-            default:
-                break;
-        }
-    } 
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'touchDown'");
+    }
 
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'touchUp'");
+    }
+
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'touchCancelled'");
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'touchDragged'");
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseMoved'");
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'scrolled'");
+    }
     
-
 }
---
+++ src/main/java/inf112/skeleton/controller/gameKeys.java
@@ -0,0 +1,29 @@
+package inf112.skeleton.controller;
+
+import com.badlogic.gdx.Input;
+
+public enum gameKeys {
+    //Menu keys
+    PAUSE (Input.Keys.ESCAPE),
+    BACK (Input.Keys.BACKSPACE),
+
+    //Movement keys
+    UP (Input.Keys.W, Input.Keys.UP),
+    DOWN (Input.Keys.S, Input.Keys.DOWN),
+    LEFT (Input.Keys.A, Input.Keys.LEFT), 
+    RIGHT (Input.Keys.D, Input.Keys.RIGHT), 
+
+    //Player interaction keys
+    INTERACT (Input.Keys.E, Input.Keys.ENTER),
+    ;
+
+    final int[] keyCode;
+    
+    private gameKeys(int... keyCode) {
+        this.keyCode = keyCode;
+    }
+
+    public int[] getKeyCode() {
+        return keyCode;
+    }
+}
