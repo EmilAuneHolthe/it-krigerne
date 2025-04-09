43be8d87307212217c35f47857b74c812bde326a
Vemund Handeland <vemundhandeland@gmail.com>
2025-04-09 02:25:14 +0200

    la til en healthbar over alle fiender

--- src/main/java/inf112/skeleton/model/entity/Enemy.java
+++ src/main/java/inf112/skeleton/model/entity/Enemy.java
@@ -23,93 +23,103 @@ public class Enemy implements entity {
     public Texture playerIdleFrontTexture, playerIdleUpTexture, playerIdleRightTexture, playerIdleLeftTexture;
     private Sprite playerSprite;
     private String name;
-    public Enemy(GamePanel context, World world, Body body, int health, int damage, float x, float y, String name) {
-        this.context = context;
-        this.world = world;
-        this.body = body;
-        this.health = health;
-        this.damage = damage;
-        this.x = x;
-        this.y = y;
-        this.name = name;
-        direction = "Front";
-        this.speed = 2f;
-    }
-    @Override
-    public int attack() {
-      // TODO Auto-generated method stub
-      throw new UnsupportedOperationException("Unimplemented method 'attack'");
-    }
-    @Override
-    public void die() {
-      // TODO Auto-generated method stub
-      throw new UnsupportedOperationException("Unimplemented method 'die'");
-    }
-
-    @Override
-    public int getHealth() {
-      if (health < 0) {
-        return 0;
-      }
-      return health;
-    }
-    @Override
-    public boolean takeDamage(int damage) {
-      health -= damage;
-      if (health < 1) {
-        int index = context.getEnemy().indexOf(this, true);
-        if (index >= 0) {
-            // Destroy the Box2D body
-            world.destroyBody(body);
-            // Remove from the enemy list
-            context.getEnemy().removeIndex(index);
-            System.out.println("Enemy removed from list: " + name);
+    private Texture healthBarTexture;
+    private  Texture healthTexture;
+    private Texture backgroundTexture;
+        public Enemy(GamePanel context, World world, Body body, int health, int damage, float x, float y, String name) {
+            this.context = context;
+            this.world = world;
+            this.body = body;
+            this.health = health;
+            this.damage = damage;
+            this.x = x;
+            this.y = y;
+            this.name = name;
+            direction = "Front";
+            this.speed = 2f;
         }
-      }
-      return health > 0;
-    }
-    @Override
-    public void setHealth(int health) {
-      this.health = health;
-    }
-    @Override
-    public void setSpawn(float x, float y) {
-      this.x = x;
-      this.y = y;
-    }
-    @Override
-    public float getX() {
-     return x;
-    }
-    @Override
-    public float getY() {
-     return y;
-    }
-    public void create(int health, int damage, float x, float y) {
-      this.health = health;
-      this.damage = damage;
-      this.x = x;
-      this.y = y;
-    }
-    @Override
-    public Body getBody() {
-      return body;
-    }
-        public void loadTextures(){
-        playerIdleFrontTexture = new Texture(Gdx.files.internal("Player/PlayerIdle/PlayerIdleFront.png"));
-        playerIdleUpTexture = new Texture(Gdx.files.internal("Player/PlayerIdle/PlayerIdleUp.png"));
-        playerIdleLeftTexture = new Texture(Gdx.files.internal("Player/PlayerIdle/PlayerIdleLeft.png"));
-        playerIdleRightTexture = new Texture(Gdx.files.internal("Player/PlayerIdle/PlayerIdleRight.png"));
-        playerSprite = new Sprite(playerIdleFrontTexture);
-        playerSprite.setSize(1, 1);
-        playerSprite.setTexture(playerIdleFrontTexture);
-    }
-        public void render(SpriteBatch batch) {
-        playerSprite.setPosition(body.getPosition().x - playerSprite.getWidth() / 2, 
-        body.getPosition().y - playerSprite.getHeight() / 2);
-        batch.begin();
-        playerSprite.draw(batch);
-        batch.end();
+        @Override
+        public int attack() {
+          // TODO Auto-generated method stub
+          throw new UnsupportedOperationException("Unimplemented method 'attack'");
+        }
+        @Override
+        public void die() {
+          // TODO Auto-generated method stub
+          throw new UnsupportedOperationException("Unimplemented method 'die'");
+        }
+    
+        @Override
+        public int getHealth() {
+          if (health < 0) {
+            return 0;
+          }
+          return health;
+        }
+        @Override
+        public boolean takeDamage(int damage) {
+          health -= damage;
+          if (health < 1) {
+            int index = context.getEnemy().indexOf(this, true);
+            if (index >= 0) {
+                // Destroy the Box2D body
+                world.destroyBody(body);
+                // Remove from the enemy list
+                context.getEnemy().removeIndex(index);
+                System.out.println("Enemy removed from list: " + name);
+            }
+          }
+          return health > 0;
+        }
+        @Override
+        public void setHealth(int health) {
+          this.health = health;
+        }
+        @Override
+        public void setSpawn(float x, float y) {
+          this.x = x;
+          this.y = y;
+        }
+        @Override
+        public float getX() {
+         return x;
+        }
+        @Override
+        public float getY() {
+         return y;
+        }
+        public void create(int health, int damage, float x, float y) {
+          this.health = health;
+          this.damage = damage;
+          this.x = x;
+          this.y = y;
+        }
+        @Override
+        public Body getBody() {
+          return body;
+        }
+            public void loadTextures(){
+            playerIdleFrontTexture = new Texture(Gdx.files.internal("Player/PlayerIdle/PlayerIdleFront.png"));
+            playerIdleUpTexture = new Texture(Gdx.files.internal("Player/PlayerIdle/PlayerIdleUp.png"));
+            playerIdleLeftTexture = new Texture(Gdx.files.internal("Player/PlayerIdle/PlayerIdleLeft.png"));
+            playerIdleRightTexture = new Texture(Gdx.files.internal("Player/PlayerIdle/PlayerIdleRight.png"));
+            healthBarTexture = new Texture(Gdx.files.internal("Player/PlayerIdle/PlayerIdleFront.png"));
+            playerSprite = new Sprite(playerIdleFrontTexture);
+            playerSprite.setSize(1, 1);
+            playerSprite.setTexture(playerIdleFrontTexture);
+        }
+            public void render(SpriteBatch batch) {
+            playerSprite.setPosition(body.getPosition().x - playerSprite.getWidth() / 2, 
+            body.getPosition().y - playerSprite.getHeight() / 2);
+            batch.begin();
+            playerSprite.draw(batch);
+            batch.end();
+            healthTexture = new Texture(Gdx.files.internal("redtexture.png"));
+            backgroundTexture = new Texture(Gdx.files.internal("graytexture.png"));
+            batch.begin();
+            batch.draw(backgroundTexture, body.getPosition().x - playerSprite.getWidth() / 2, body.getPosition().y + playerSprite.getHeight() / 2, 1, 0.1f);
+            batch.draw(healthTexture, body.getPosition().x - playerSprite.getWidth() / 2, body.getPosition().y + playerSprite.getHeight() / 2, 1 * (health / 100f), 0.1f);
+            batch.end();
     }
     public Vector2 getPosition() {
         return new Vector2(body.getPosition().x, body.getPosition().y);
