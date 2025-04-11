8a658729c9fe5d50604552fbbc9ef6010cd363e1
Emhol8004 <Emhol8004@uib.no>
2025-04-11 23:31:22 +0200

    implementerte tomme testklasser

--- src/test/java/inf112/skeleton/MainTest.java
+++ src/test/java/inf112/skeleton/MainTest.java
@@ -1,11 +1,20 @@
 package inf112.skeleton;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @Test
-    void testSkeleton() {
-        assertTrue(true);
+    void testMainClassExists() {
+        assertNotNull(Main.class, "Main class should exist");
+    }
+
+    @Test
+    void testMainMethodExists() {
+        try {
+            Main.class.getMethod("main", String[].class);
+        } catch (NoSuchMethodException e) {
+            fail("Main class should have a main method");
+        }
     }
 }
