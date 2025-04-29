c274657c7dc0cf60f798475317ada845d54c48c6
Emhol8004 <Emhol8004@uib.no>
2025-04-29 15:27:01 +0200

    javadoc på entity interface

--- src/main/java/inf112/skeleton/model/entity/entity.java
+++ src/main/java/inf112/skeleton/model/entity/entity.java
@@ -2,25 +2,82 @@ package inf112.skeleton.model.entity;
 
 import com.badlogic.gdx.physics.box2d.Body;
 
+/**
+ * Represents a game entity with basic attributes and actions.
+ */
 public interface entity {
-    //should return the amount of damage the entity does
+
+    /**
+     * Returns the amount of damage the entity does.
+     *
+     * @return the damage value
+     */
     int attack();
-    //should set the entity's health to 0 and remove it from the board
+
+    /**
+     * Sets the entity's health to 0 and removes it from the board.
+     */
     void die();
-    //should return the entity's health
+
+    /**
+     * Returns the entity's current health.
+     *
+     * @return the health value
+     */
     int getHealth();
-    //should return true if the entity's health is above 0, false otherwise
+
+    /**
+     * Reduces the entity's health by the given damage amount.
+     * Returns true if the entity is still alive (health > 0), false otherwise.
+     *
+     * @param damage the amount of damage to apply
+     * @return true if the entity is alive, false otherwise
+     */
     boolean takeDamage(int damage);
-    //should set the entity's health to the given value
+
+    /**
+     * Sets the entity's health to the given value.
+     *
+     * @param health the new health value
+     */
     void setHealth(int health);
-    //should set the entity's spawn point to the given coordinates
+
+    /**
+     * Sets the entity's spawn point to the given coordinates.
+     *
+     * @param x the x-coordinate of the spawn point
+     * @param y the y-coordinate of the spawn point
+     */
     void setSpawn(float x, float y);
-    //should return the x-coordinate of the entity
+
+    /**
+     * Returns the x-coordinate of the entity.
+     *
+     * @return the x-coordinate
+     */
     float getX();
-    //should return the y-coordinate of the entity
+
+    /**
+     * Returns the y-coordinate of the entity.
+     *
+     * @return the y-coordinate
+     */
     float getY();
-    //create a new entity
+
+    /**
+     * Creates a new entity with the specified attributes.
+     *
+     * @param health the initial health of the entity
+     * @param damage the damage the entity can deal
+     * @param x the x-coordinate of the entity's spawn point
+     * @param y the y-coordinate of the entity's spawn point
+     */
     void create(int health, int damage, float x, float y);
-    //should return the entity's body
+
+    /**
+     * Returns the entity's physical body.
+     *
+     * @return the {@link Body} of the entity
+     */
     Body getBody();
 }
