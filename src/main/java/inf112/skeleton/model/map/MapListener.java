2c96e215854ac027f234adb4985920f846e60a22
Vemund Handeland <vemundhandeland@gmail.com>
2025-05-02 17:14:02 +0200

    har lagd Java Docs for MapListener

--- src/main/java/inf112/skeleton/model/map/MapListener.java
+++ src/main/java/inf112/skeleton/model/map/MapListener.java
@@ -1,5 +1,15 @@
 package inf112.skeleton.model.map;
 
+/**
+ * Interface for objects that need to be notified when the game map changes.
+ * Implement this interface to receive notifications about map transitions.
+ */
 public interface MapListener {
+    /**
+     * Called when the game map changes to a new map.
+     * Implement this method to handle map transition events.
+     *
+     * @param map The new map that has been loaded
+     */
     void mapChanged(final Map map);
 }
